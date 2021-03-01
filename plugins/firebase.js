b08e1e97eacf1ae629eff365503262b132a717cc
kanoi-y <furafuratonbo@gmail.com>
2021-03-01 17:36:23 +0900

    リポジトリを作成

---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(
+      {
+        apiKey: "AIzaSyBYJuEewF5fu2L0OqFp7QzjUUDPs0mwblA",
+        authDomain: "cotucotu-788dd.firebaseapp.com",
+        projectId: "cotucotu-788dd",
+        storageBucket: "cotucotu-788dd.appspot.com",
+        messagingSenderId: "95257492842",
+        appId: "1:95257492842:web:f55f4601d8384ea9d93913",
+        measurementId: "G-M0HDCDS5XR"
+      }
+    )
+  }
+  
+  export default firebase
